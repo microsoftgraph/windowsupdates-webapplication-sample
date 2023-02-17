dc8ab597ced8739133eccb839be83d3910815bdd
MrWyss-MSFT <marius.wyss@microsoft.com>
2023-02-17 22:42:35 +0100

    consistent spelling

--- src/config/AuthConfig.js
+++ src/config/AuthConfig.js
@@ -2,7 +2,7 @@ import { LogLevel } from "@azure/msal-browser"; //MIT LICENSE < https://github.c
 
 export const msalConfig = {
   auth: {
-    clientId: "CLIENT ID", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
+    clientId: "CLIENT_ID", // e.g. "70258689-8a4e-410f-a300-cb2011f23cf3"
     authority: "https://login.windows.net/TENANT_ID", //e.g. "https://login.windows.net/4d67e6d4-78f7-438b-8e40-7e408dcfa0ca"
     redirectUri: "http://localhost:3000/",
   },
